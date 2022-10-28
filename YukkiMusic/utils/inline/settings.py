2143b985b5bd28ecb4d849a844edede62333d7ac
Lagon <109609208+codeslagon@users.noreply.github.com>
2022-10-29 01:50:55 +0300

    Update settings.py

--- YukkiMusic/utils/inline/settings.py
+++ YukkiMusic/utils/inline/settings.py
@@ -27,7 +27,7 @@ def setting_markup(_):
                 text=_["ST_B_3"], callback_data="AU"
             ),
             InlineKeyboardButton(
-                text=_["ST_B_6"], callback_data="LG"
+                text=_["ST_B_6"], url=f"{SUPPORT_GROUP}"
             ),
         ],
         [
